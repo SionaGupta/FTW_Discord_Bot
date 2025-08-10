bf436382177c5443f27441b79511d209eb217110
SionaGupta <siona.gupta00@gmail.com>
2025-08-10 13:03:38 -0700

    Changed Variables
    Made Webhook URL hidden

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+url = os.getenv('WEBHOOK_URL')
+
 
 app = Flask(__name__)
 
-D_WH_URL = "https://discord.com/api/webhooks/1402781367380475935/JbIOx-7jQXzv-EB4K3htPmPI1knO4nIbkoILSAgza6MQL7Y7rKP_W9iGL_wSKEn4nZJF"
-
+url = 
 @app.route("/"):
     
